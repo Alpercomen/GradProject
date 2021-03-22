70ee0f0305c5770bc9d2e615a0bcdccebe8f287c
Alpercomen <comenalper@gmail.com>
2021-03-23 00:29:32 +0300

    1.1

--- main.py
+++ main.py
@@ -9,21 +9,30 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from kivy.properties import BooleanProperty
 
 #Change background color to white
 Window.clearcolor = (1, 1, 1, 0.9)
 
 class MainWindow(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(MainWindow, self).__init__(**kwargs)
 
 class SecondWindow(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(SecondWindow, self).__init__(**kwargs)
+        
+    answer1 = BooleanProperty(False)
+    answer2 = BooleanProperty(False)
+    answer3 = BooleanProperty(False)
 
 class ThirdWindow(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(ThirdWindow, self).__init__(**kwargs)
 
 class WindowManager(ScreenManager):
-    pass
+    def __init__(self, **kwargs):
+        super(WindowManager, self).__init__(**kwargs)
 
 kv = Builder.load_file("dpbs.kv")
     
