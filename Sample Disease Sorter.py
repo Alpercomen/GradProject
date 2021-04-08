d3ffbe13c41546e12d97364e86355339fb94a380
Alpercomen <comenalper@gmail.com>
2021-04-09 01:34:58 +0300

    1.3

--- Sample Disease Sorter.py
+++ Sample Disease Sorter.py
@@ -11,29 +11,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 #Import medical dataset
-dataset = pd.read_csv("D:\\OneDrive\\Desktop\\Programming\\Python Scripts\\Kivy\\GradProject\\Datasets\\sydi.csv",names=['sid','scui','symptom','did','dcui','dicpc','disease','d1','dp','acute'])
-dataset['d1'] = dataset['d1'].fillna(0)
-dataset['dp'] = dataset['dp'].fillna(0)
-dataset['acute'] = dataset['acute'].fillna(0)
+dataset = pd.read_csv("D:\\OneDrive\\Desktop\\Programming\\Python Scripts\\Kivy\\GradProject\\Datasets\\Dataset_v4.csv",names=['symptom', 'disease'])
 
 #Split into symptoms and diseases
-symptoms, diseases = dataset[['sid', 'symptom', 'd1', 'dp', 'acute']], dataset[['did', 'disease']]
+symptoms, diseases = dataset[['symptom']], dataset[['disease']]
 
 #Normalizing the dataset, changing nominal values into numerical values
 symptoms_norm = pd.get_dummies(symptoms, drop_first=True)
 diseases_norm = pd.get_dummies(diseases, drop_first=True)
 
-#Split into train set and test set
-X_train, X_test, y_train, y_test = train_test_split(symptoms_norm, diseases_norm['disease_Abdominal swelling'], train_size= 0.7, test_size=0.3, shuffle=True)
-
 #Fit final model
-model = LogisticRegression(solver='saga', max_iter=100000)
-model.fit(X_train, y_train)
+model = LogisticRegression(solver='lbfgs')
 
-print(model.predict_proba(X_test))
+#Create and store user symptoms
+symptom_user = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
 
-#Score the model
-model.score(X_test, y_test)
-
-#Model Predictions
-#model.predict_proba(X_test)[0]
+for x in range(len(diseases_norm.iloc[0])):
+    #Split into train set and test set
+    X_train, X_test, y_train, y_test = train_test_split(symptoms_norm, diseases_norm.iloc[:,0], train_size= 0.7, test_size=0.3, shuffle=True)
+    model.fit(X_train, y_train)
+    features = model.predict_proba(symptom_user)
+    print(f"Probability for the {diseases_norm.columns.values[x]}:\n {features}\n\n")
