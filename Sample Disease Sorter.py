860517901e87a0621bf9ed4cfbf76ec65a9cccb7
Alpercomen <comenalper@gmail.com>
2021-05-01 01:29:39 +0300

    2.0

--- Sample Disease Sorter.py
+++ Sample Disease Sorter.py
@@ -5,30 +5,51 @@ Created on Mon Mar 29 21:01:20 2021
 @author: alper
 """
 
+import heapq
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 #Import medical dataset
-dataset = pd.read_csv("D:\\OneDrive\\Desktop\\Programming\\Python Scripts\\Kivy\\GradProject\\Datasets\\Dataset_v4.csv",names=['symptom', 'disease'])
+dataset = pd.read_csv("D:\\OneDrive\\Desktop\\Programming\\Python Scripts\\Kivy\\GradProject\\Datasets\\Dataset_v5.csv",names=['symptom', 'disease'])
 
 #Split into symptoms and diseases
 symptoms, diseases = dataset[['symptom']], dataset[['disease']]
 
 #Normalizing the dataset, changing nominal values into numerical values
 symptoms_norm = pd.get_dummies(symptoms, drop_first=True)
+symptoms_norm.columns = symptoms_norm.columns.str.replace("symptom_", "")
 diseases_norm = pd.get_dummies(diseases, drop_first=True)
+diseases_norm.columns = diseases_norm.columns.str.replace("disease_", "")
 
 #Fit final model
 model = LogisticRegression(solver='lbfgs')
 
 #Create and store user symptoms
-symptom_user = np.zeros(shape=32).reshape(1,-1)
+symptom_user = np.zeros(shape=len(symptoms_norm.columns)).reshape(1,-1)
 
-for x in range(len(diseases_norm.iloc[0])):
-    #Split into train set and test set
-    X_train, X_test, y_train, y_test = train_test_split(symptoms_norm, diseases_norm.iloc[:,0], train_size= 0.7, test_size=0.3, shuffle=True)
+probabilities = [None] * len(diseases_norm.columns)
+
+for disease_prob in range(len(diseases_norm.iloc[0])):
+    # Split into train set and test set
+    X_train, X_test, y_train, y_test = train_test_split(symptoms_norm, diseases_norm.iloc[:, 0], train_size=0.7,
+                                                        test_size=0.3, shuffle=True)
     model.fit(X_train, y_train)
     features = model.predict_proba(symptom_user)
-    print(f"Probability for the {diseases_norm.columns.values[x]}:\n {features}\n\n")
+    probabilities[disease_prob] = int(features[0, 1] * 100)
+    print(f"Probability for the {diseases_norm.columns.values[disease_prob]}:\n{round(features[0, 1] * 100, 2)}%\n\n")
+
+prob_indices = heapq.nlargest(5, range(len(probabilities)), probabilities.__getitem__)
+max_prob = [None] * 5
+max_disease = [None] * 5
+
+for index in range(len(prob_indices)):
+    max_prob[index] = probabilities[prob_indices[index]]
+    max_disease[index] = diseases_norm.columns.values[prob_indices[index]]
+
+plt.pie(max_prob, labels=max_disease, autopct='%1.1f%%')
+plt.title('Probabilities')
+plt.axis('equal')
+plt.show()
--- main.py
+++ main.py
@@ -12,84 +12,141 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.properties import BooleanProperty
 
-#Change background color to white
-Window.clearcolor = (1, 1, 1, 0.9)
+# OTHER IMPORTS
+import heapq
+import os
 
 # NUMPY AND PANDAS
 import numpy as np
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
+# MATPLOTLIB
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
 # SCIKIT-LEARN IMPORTS
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+#Change background color to white
+Window.clearcolor = (1, 1, 1, 0.9)
+
+#Import medical dataset
+dataset = pd.read_csv("D:\\OneDrive\\Desktop\\Programming\\Python Scripts\\Kivy\\GradProject\\Datasets\\Dataset_v6.csv",names=['symptom', 'disease'])
+
+#Split into symptoms and diseases
+symptoms, diseases = dataset[['symptom']], dataset[['disease']]
+
+#Normalizing the dataset, changing nominal values into numerical values
+symptoms_norm = pd.get_dummies(symptoms, drop_first=True)
+symptoms_norm.columns = symptoms_norm.columns.str.replace("symptom_", "")
+diseases_norm = pd.get_dummies(diseases, drop_first=True)
+diseases_norm.columns = diseases_norm.columns.str.replace("disease_", "")
+
 #Create an empy user symptom
-symptom_user = np.zeros(shape=32).reshape(1,-1)
+symptom_user = np.zeros(shape=len(symptoms_norm.columns)).reshape(1,-1)
+probabilities = [None] * len(diseases_norm.columns)
 
-class MainWindow(Screen):
-    def __init__(self, **kwargs):
-        super(MainWindow, self).__init__(**kwargs)
-
-class SecondWindow(Screen):
+#Abstract window
+class Window(Screen):
     answer1 = BooleanProperty(False)
     answer2 = BooleanProperty(False)
     answer3 = BooleanProperty(False)
-    
-    def __init__(self, **kwargs):
-        super(SecondWindow, self).__init__(**kwargs)
-        
-    def set_value(self, index, value):
-        """Sets the given index to a value"""
-        symptom_user[0, index] = value
-        print(f"Symptom[{index}] = {symptom_user[0, index]}")
-    
-    
-
-class ThirdWindow(Screen):
     answer4 = BooleanProperty(False)
     answer5 = BooleanProperty(False)
     answer6 = BooleanProperty(False)
-    
+
     def __init__(self, **kwargs):
-        super(ThirdWindow, self).__init__(**kwargs)
-        
+        super(Window, self).__init__(**kwargs)
+
     def set_value(self, index, value):
         """Sets the given index to a value"""
         symptom_user[0, index] = value
-        print(f"Symptom[{index}] = {symptom_user[0, index]}")
-    
+        print(f"{symptoms_norm.columns[index]} = {symptom_user[0, index]}")
+
+    def print_symptom(self, index):
+        return symptoms_norm.columns[index]
+
+    def reset_answer(self):
+        self.answer1 = BooleanProperty(False)
+        self.answer2 = BooleanProperty(False)
+        self.answer3 = BooleanProperty(False)
+        self.answer4 = BooleanProperty(False)
+        self.answer5 = BooleanProperty(False)
+        self.answer6 = BooleanProperty(False)
+
+class FirstWindow(Window):
+    pass
+
+class SecondWindow(Window):
+    pass
+
+class ThirdWindow(Window):
+    pass
+
+class FourthWindow(Window):
+    pass
+
+class FifthWindow(Window):
+    pass
+
+class SixthWindow(Window):
+    pass
+
+class SeventhWindow(Window):
+    pass
+
+class EighthWindow(Window):
+    pass
+
+class NinthWindow(Window):
     def train(self):
         """Training"""
-        #Import medical dataset
-        dataset = pd.read_csv("D:\\OneDrive\\Desktop\\Programming\\Python Scripts\\Kivy\\GradProject\\Datasets\\Dataset_v4.csv",names=['symptom', 'disease'])
-        
-        #Split into symptoms and diseases
-        symptoms, diseases = dataset[['symptom']], dataset[['disease']]
-        
-        #Normalizing the dataset, changing nominal values into numerical values
-        symptoms_norm = pd.get_dummies(symptoms, drop_first=True)
-        diseases_norm = pd.get_dummies(diseases, drop_first=True)
-        
-        #Fit final model
+        # Fit final model
         model = LogisticRegression(solver='lbfgs')
-            
         for disease_prob in range(len(diseases_norm.iloc[0])):
-            #Split into train set and test set
-            X_train, X_test, y_train, y_test = train_test_split(symptoms_norm, diseases_norm.iloc[:,0], train_size= 0.7, test_size=0.3, shuffle=True)
+            # Split into train set and test set
+            X_train, X_test, y_train, y_test = train_test_split(symptoms_norm, diseases_norm.iloc[:, 0], train_size=0.7,
+                                                                test_size=0.3, shuffle=True)
             model.fit(X_train, y_train)
             features = model.predict_proba(symptom_user)
-            print(f"Probability for the {diseases_norm.columns.values[disease_prob]}:\n {features}\n\n")   
-        
-        
-class FinalWindow(Screen):
-    def __init__(self, **kwargs):
-        super(FinalWindow, self).__init__(**kwargs)    
-    
+            probabilities[disease_prob] = int(features[0, 1] * 10000)
+            print(f"Probability for the {diseases_norm.columns.values[disease_prob]}:\n{round(features[0, 1] * 100, 2)}%")
+
+        prob_indices = heapq.nlargest(5, range(len(probabilities)), probabilities.__getitem__)
+        max_prob = [None] * 5
+        max_disease = [None] * 5
+
+        for index in range(len(prob_indices)):
+            max_prob[index] = probabilities[prob_indices[index]]
+            max_disease[index] = diseases_norm.columns.values[prob_indices[index]]
+
+        # Set font size
+        mpl.rcParams['font.size'] = 9.0
+
+        # Colors
+        colors = ['#8870FF', '#6DD582', '#D2D56D', '#D57E6D', '#D26DD5']
+
+        fig, ax = plt.subplots()
+        ax.pie(max_prob, colors=colors, labels=max_disease, autopct='%1.1f%%', startangle=90, pctdistance=0.85)
+
+        # Draw circle
+        centre_circle = plt.Circle((0, 0), 0.85, fc='white')
+        fig2 = plt.gcf()
+        fig2.gca().add_artist(centre_circle)
+
+        if (os.path.exists('train_results.png')):
+            os.remove('train_results.png')
+        ax.axis('equal')
+        plt.tight_layout()
+        plt.savefig('train_results.png')
+        plt.show()
+
+class FinalWindow(Window):
+    pass
 
 class WindowManager(ScreenManager):
-    def __init__(self, **kwargs):
-        super(WindowManager, self).__init__(**kwargs)
-        
+    pass
     
 
 kv = Builder.load_file("dpbs.kv")
